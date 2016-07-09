8f41822c57de1ad7a6a4547438b1cfdeddf0fc78
Abhinav Modi <abhinavmodi@gmail.com>
2016-07-09 05:24:52 -0700

    Initial Commit
    Initial Commit

--- example_app.py
+++ example_app.py
@@ -1,26 +1,28 @@
 from example_samplers import *
-from apic import ApicTemp, ApicHealth, TenantHealth, NetworkEndpointsCount, EpgStats, EpgStatsFake, FabricMembersHealth, EpgEndPointsFake, AllTenants
+from apic import  CliCPU, CliIP
+
+leaf1 = '10.126.216.230'
+leaf2 = '10.126.216.231'
+spine1 = '10.126.216.71'
+
 
 def run(app, xyzzy):
     samplers = [
         SynergySampler(xyzzy, 3),
         BuzzwordsSampler(xyzzy, 2),
         ConvergenceSampler(xyzzy, 1),
-        ApicTemp(xyzzy,5),
-        ApicHealth(xyzzy,5),
-        TenantHealth(app=xyzzy, interval=5, tenant = 'L2-Test'),
-        TenantHealth(app=xyzzy, interval=5, tenant='infra'),
-        NetworkEndpointsCount(app=xyzzy, interval=5, tenant='L2-Test', anp='L2-Test', epg='VLAN96', netmax=254),
-        EpgStats(app=xyzzy, interval=30, tenant='L2-Test', anp='L2-Test', epg='VLAN96'),
-        FabricMembersHealth(app=xyzzy, interval=10 ),
-        EpgStatsFake(xyzzy,5),
-        EpgEndPointsFake(xyzzy, 10),
-        AllTenants(xyzzy, 10)
+        CliCPU(xyzzy,1,leaf1,'cli_cpu_l1',1),
+        CliCPU(xyzzy,1,leaf2,'cli_cpu_l2',1),
+        CliCPU(xyzzy,1,spine1,'cli_cpu_s1',0),
+        CliIP(xyzzy,5,spine1,'cli_ip_s1',1),
+        CliIP(xyzzy,5,leaf1,'cli_ip_l1',1),
+        CliIP(xyzzy,5,leaf2,'cli_ip_l2',1)
     ]
 
     try:
         app.run(debug=True,
                 port=5000,
+                host='0.0.0.0',
                 threaded=True,
                 use_reloader=False,
                 use_debugger=True
